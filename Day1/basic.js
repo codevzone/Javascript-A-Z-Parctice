abae657fcdd97369185e624289be376f7e0415cf
Satya Mishra <Satya.Mishra@flydocs.aero>
2024-10-06 09:17:46 +0530

    feat: basic understanding of the js

---
+++ Day1/basic.js
@@ -0,0 +1,17 @@
+// this is the single line comment
+
+/*
+multiple line
+sefasdf
+of comment
+*/
+
+// JS Doc..
+/**
+ * Represents a book.
+ * @deprecated - this funciton working like that....
+ */
+function Book(title, author) {
+}
+
+//  Book();
