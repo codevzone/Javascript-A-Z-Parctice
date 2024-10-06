af29c4bc6572da6604224ebac3f69eee87225c38
Satya Mishra <Satya.Mishra@flydocs.aero>
2024-10-06 10:01:49 +0530

    topic: operators

---
+++ Day1/operators.js
@@ -0,0 +1,44 @@
+// Assignment Operators;
+// let calculatedData = 1+2;
+// let calculatedData = 2*2;
+let calculatedData = 10;
+// console.log(calculatedData)
+
+// calculatedData+=1; // 11
+// console.log(calculatedData)
+
+// calculatedData*=2; // 11 - calculatedData = calculatedData * 2;
+// console.log(calculatedData)
+
+// calculatedData++; // 11
+// console.log(calculatedData)
+
+// calculatedData--; // 9
+// console.log(calculatedData)
+
+
+
+//Comparison Operators:
+// const a = 1
+// const b = '1'
+// console.log(a==b)// true
+// console.log(a===b)// false
+
+// console.log(a!=b)// false
+// console.log(a!==b)// true
+
+
+//Logical Operator
+let num1 = 0;
+const num2 = 33;
+// console.log(Boolean(num1)&&Boolean(num2))//false
+
+// console.log(num1 && num2)//0
+
+// num1 = 1;
+// console.log(num1 && num2)//33
+
+
+// OR
+// console.log(true || false)//true
+// console.log(!false) //true
