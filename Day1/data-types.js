b7d9a87e929e704d73d79670d4190e129ff429e4
Satya Mishra <Satya.Mishra@flydocs.aero>
2024-10-06 09:43:11 +0530

    topic: data type

---
+++ Day1/data-types.js
@@ -0,0 +1,45 @@
+// Primitive Data Types:
+// Number
+// console.log(Number.MAX_VALUE) // greatest of the 0
+// console.log(Number.MIN_VALUE) // lowest of the 0
+// console.log(Number.isInteger('codevzone'))  // false
+
+// console.log(Number('044'))  // 44
+// console.log(Number('satya'))  // NaN
+// console.log(Number.MAX_SAFE_INTEGER) //9007199254740991
+// console.log(Number.MIN_SAFE_INTEGER) //--9007199254740991
+
+ 
+
+// Boolean
+// console.log(Boolean('fasdf'))  // true
+// console.log(Boolean(null))  // false
+// console.log(Boolean(undefined))  // false
+
+
+// null and undefined difference 
+// let name;
+// console.log(name) // undefined
+// name = null;
+// console.log(name) // null
+
+
+//Primitive data type
+// let name = 'satya';
+// let name2 = name;
+// console.log(name2,name) // 'satya' // 'satya'
+
+// name2 = 'codevzone'
+// console.log(name,name2) // 'name' // 'codevzone'
+
+// Referenced data type
+// let personDetails = {
+//     name: 'satya',
+//     age: 25
+// }
+
+// console.dir(personDetails)
+
+// let modifiedData = personDetails;
+// modifiedData.name = 'codevzone'
+// console.dir(personDetails)
